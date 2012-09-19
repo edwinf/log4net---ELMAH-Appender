7f8d70657b8dc842dc1e872e57ee8e6f2ffbc5a6
Ed Frey <crazyoatmeal@gmail.com>
2012-09-19 15:23:36 -0400

    Now it's functional

--- ELMAH Appender for log4net/ELMAHAppender.cs
+++ ELMAH Appender for log4net/ELMAHAppender.cs
@@ -8,15 +8,15 @@ using System.Web;
 
 namespace elmahappender_log4net
 {
-    public class ELMAHAppender : log4net.Appender.AppenderSkeleton
-    {
+	public class ELMAHAppender : log4net.Appender.AppenderSkeleton
+	{
 		private readonly static Type _DeclaringType = typeof(ELMAHAppender);
 		private string _HostName;
 		private ErrorLog _ErrorLog;
 
 
 		public override void ActivateOptions()
-		{	
+		{
 			base.ActivateOptions();
 			_HostName = Environment.MachineName;
 			try
@@ -33,16 +33,26 @@ namespace elmahappender_log4net
 		{
 			if (this._ErrorLog != null)
 			{
-				Error error = new Error(loggingEvent.ExceptionObject);
-				error.ApplicationName = ""; //configurable app name?
+				Error error;
+				if (loggingEvent.ExceptionObject != null)
+				{
+					error = new Error(loggingEvent.ExceptionObject);
+				}
+				else
+				{
+					error = new Error();
+				}
+				error.Time = DateTime.Now;
+				if (loggingEvent.MessageObject != null)
+				{
+					error.Message = loggingEvent.MessageObject.ToString();
+				}
 				error.Detail = base.RenderLoggingEvent(loggingEvent);
 				error.HostName = this._HostName;
 				error.User = loggingEvent.Identity;
-				error.Type = "Custom - " + loggingEvent.Level; // maybe allow the type to be customized?
-				//error.Message = loggingEvent.m
-
+				error.Type = "log4net - " + loggingEvent.Level; // maybe allow the type to be customized?
 				this._ErrorLog.Log(error);
 			}
 		}
-    }
+	}
 }
