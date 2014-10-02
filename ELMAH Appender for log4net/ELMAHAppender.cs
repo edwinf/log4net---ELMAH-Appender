5d710470e70274d2f54fc0b0713c63b262c0dfc3
rcollette@yahoo.com <rcollette@yahoo.com>
2014-10-02 10:41:38 -0400

    Remove unused static member.  Set error time to log4net time.   Set source to loggerName.  Set type to logging.Event.Level.

--- ELMAH Appender for log4net/ELMAHAppender.cs
+++ ELMAH Appender for log4net/ELMAHAppender.cs
@@ -1,65 +1,62 @@
 ﻿using Elmah;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Web;
 
-namespace elmahappender_log4net
+namespace Hsb.Log4Net.Appender
 {
-	public class ELMAHAppender : log4net.Appender.AppenderSkeleton
-	{
-		private readonly static Type _DeclaringType = typeof(ELMAHAppender);
-		private string _HostName;
-		private ErrorLog _ErrorLog;
+    public class ELMAHAppender : log4net.Appender.AppenderSkeleton
+    {
+        private string _HostName;
+        private ErrorLog _ErrorLog;
 
-		public bool UseNullContext { get; set; }
+        public bool UseNullContext { get; set; }
 
-		public override void ActivateOptions()
-		{
-			base.ActivateOptions();
-			_HostName = Environment.MachineName;
-			try
-			{
-				if (UseNullContext)
-				{
-					this._ErrorLog = ErrorLog.GetDefault(null);
-				}
-				else
-				{
-					this._ErrorLog = ErrorLog.GetDefault(HttpContext.Current);
-				}
-			}
-			catch (Exception ex)
-			{
-				this.ErrorHandler.Error("Could not create default ELMAH error log", ex);
-			}
-		}
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+            _HostName = Environment.MachineName;
+            try
+            {
+                if (UseNullContext)
+                {
+                    this._ErrorLog = ErrorLog.GetDefault(null);
+                }
+                else
+                {
+                    this._ErrorLog = ErrorLog.GetDefault(HttpContext.Current);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.ErrorHandler.Error("Could not create default ELMAH error log", ex);
+            }
+        }
 
-
-		protected override void Append(log4net.Core.LoggingEvent loggingEvent)
-		{
-			if (this._ErrorLog != null)
-			{
-				Error error;
-				if (loggingEvent.ExceptionObject != null)
-				{
-					error = new Error(loggingEvent.ExceptionObject);
-				}
-				else
-				{
-					error = new Error();
-				}
-				error.Time = DateTime.Now;
-				if (loggingEvent.MessageObject != null)
-				{
-					error.Message = loggingEvent.MessageObject.ToString();
-				}
-				error.Detail = base.RenderLoggingEvent(loggingEvent);
-				error.HostName = this._HostName;
-				error.User = loggingEvent.Identity;
-				error.Type = "log4net - " + loggingEvent.Level; // maybe allow the type to be customized?
-				this._ErrorLog.Log(error);
-			}
-		}
-	}
+        protected override void Append(log4net.Core.LoggingEvent loggingEvent)
+        {
+            if (this._ErrorLog != null)
+            {
+                Error error;
+                if (loggingEvent.ExceptionObject != null)
+                {
+                    error = new Error(loggingEvent.ExceptionObject);
+                }
+                else
+                {
+                    error = new Error();
+                }
+                error.Time = loggingEvent.TimeStamp;
+                if (loggingEvent.MessageObject != null)
+                {
+                    error.Message = loggingEvent.MessageObject.ToString();
+                }
+                error.Detail = base.RenderLoggingEvent(loggingEvent);
+                error.HostName = this._HostName;
+                error.User = loggingEvent.Identity;
+                error.Source = loggingEvent.LoggerName;
+                error.Type = loggingEvent.Level.Name;
+                this._ErrorLog.Log(error);
+            }
+        }
+    }
 }
