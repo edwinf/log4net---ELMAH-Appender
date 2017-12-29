c3a528aa2917e94264f7f66d3fe89cfbdc61fcef
Jenser Almeida <jalmeida@vonigo.com>
2017-12-29 15:46:34 -0800

    Updtating readme and making some small code-style changes to elmahapeender.cs

--- ElmahAppender.log4net/ElmahAppender.cs
+++ ElmahAppender.log4net/ElmahAppender.cs
@@ -6,59 +6,47 @@ using System.Web;
 
 namespace ElmahAppender.log4net
 {
-	public class ElmahAppender : AppenderSkeleton
-	{
-		private readonly static Type _DeclaringType = typeof(ElmahAppender);
-		private string _HostName;
-		private ErrorLog _ErrorLog;
+    public class ElmahAppender : AppenderSkeleton
+    {
+        private string _hostName;
+        private ErrorLog _errorLog;
 
-		public bool UseNullContext { get; set; }
+        public bool UseNullContext { get; set; }
 
-		public override void ActivateOptions()
-		{
-			base.ActivateOptions();
-			_HostName = Environment.MachineName;
-			try
-			{
-				if (UseNullContext)
-				{
-					this._ErrorLog = ErrorLog.GetDefault(null);
-				}
-				else
-				{
-					this._ErrorLog = ErrorLog.GetDefault(HttpContext.Current);
-				}
-			}
-			catch (Exception ex)
-			{
-				this.ErrorHandler.Error("Could not create default ELMAH error log", ex);
-			}
-		}
+        public override void ActivateOptions()
+        {
+            base.ActivateOptions();
+            _hostName = Environment.MachineName;
+            try
+            {
+                _errorLog = UseNullContext
+                    ? ErrorLog.GetDefault(null)
+                    : ErrorLog.GetDefault(HttpContext.Current);
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.Error("Could not create default ELMAH error log", ex);
+            }
+        }
 
-		protected override void Append(LoggingEvent loggingEvent)
-		{
-			if (this._ErrorLog != null)
-			{
-				Error error;
-				if (loggingEvent.ExceptionObject != null)
-				{
-					error = new Error(loggingEvent.ExceptionObject);
-				}
-				else
-				{
-					error = new Error();
-				}
-				error.Time = DateTime.Now;
-				if (loggingEvent.MessageObject != null)
-				{
-					error.Message = loggingEvent.MessageObject.ToString();
-				}
-				error.Detail = base.RenderLoggingEvent(loggingEvent);
-				error.HostName = this._HostName;
-				error.User = loggingEvent.Identity;
-				error.Type = "log4net - " + loggingEvent.Level; // maybe allow the type to be customized?
-				this._ErrorLog.Log(error);
-			}
-		}
-	}
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (_errorLog == null)
+                return;
+
+            var error = loggingEvent.ExceptionObject != null
+                ? new Error(loggingEvent.ExceptionObject)
+                : new Error();
+
+            error.Time = DateTime.Now;
+            if (loggingEvent.MessageObject != null)
+                error.Message = loggingEvent.MessageObject.ToString();
+            error.Detail = RenderLoggingEvent(loggingEvent);
+            error.HostName = _hostName;
+            error.User = loggingEvent.Identity;
+            error.Type = "log4net - " + loggingEvent.Level; // maybe allow the type to be customized?
+
+            _errorLog.Log(error);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-log4net---ELMAH-Appender
+ElmahAppender.log4net
 ========================
+
+This package is a fork of https://github.com/edwinf/log4net---ELMAH-Appender and all the code was implemented by the original author. This package only modernize the .net stack of the original one.
 
 This appender will allow log4net to be configured to send log messages to ELMAH directly.  This way ELMAH can be the log manager of record for sites while still allowing specific logging parameters to be controlled with log4net as usual.
 
 Sample Log4net config:
 <pre><code>
 &lt;log4net&gt;
-    &lt;appender name="elmahappender" type="elmahappender_log4net.ELMAHAppender, elmahappender_log4net"&gt;
+    &lt;appender name="elmahappender" type="ElmahAppender.log4net.ElmahAppender, ElmahAppender.log4net"&gt;
       &lt;layout type="log4net.Layout.PatternLayout"&gt;
         &lt;conversionPattern value="%date [thread] %-5level %logger - %message%newline" /&gt;
       &lt;/layout&gt;
@@ -24,16 +26,9 @@ Sample Log4net config:
     &lt;errorLog type="Elmah.SqlErrorLog, Elmah" connectionStringName="connString" applicationName="TTDev" /&gt;
   &lt;/elmah&gt;
   </code></pre>
-  
+
   For the sql error log, I had to put the application name on the config.  For v1 I could not figure out how to pass the application name
   through the log4net log, it would ignore what I passed as the app name and just insert an empty string.  The application not matching caused the display handler to not display those log entries.
   Setting the application name in the configuration entry above solved that problem.
-  
-  This problem did not present itself with the in memory log.
 
-  <b>New for 1.0.2:</b>  A new setting was also added called UseNullContext.  This is used when you want to initialize the logger outside of a HTTP request (such as application start).  
-  Please see [this issue](https://github.com/edwinf/log4net---ELMAH-Appender/pull/2) for more details.
-  
-   <b>Note:</b> A second nuget package was created with the dependancy for log4net dropped to 1.2.10 and .Net 2.0. There were complications getting both 1.2.10 and 1.2.11 dependencies to work in the same package.
-   That second package can be found [Here](https://nuget.org/packages/elmahappender_log4net_1.2.10/)
-  
+  This problem did not present itself with the in memory log.
